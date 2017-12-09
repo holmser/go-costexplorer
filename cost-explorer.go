f38e49c5b855e55281125e455f8cb73bc5de7722
Chris Holmes <cholmes3@lifetimefitness.com>
2017-12-09 13:31:31 -0700

    This needs some love

--- cost-explorer.go
+++ cost-explorer.go
@@ -13,6 +13,16 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+var colors = map[string]int{
+	"red":    31,
+	"green":  32,
+	"yellow": 33,
+}
+
+func addColor(s string, i int) string {
+	return fmt.Sprintf("\u001b[%dm%v\u001b[0m", i, s)
+}
+
 // getDates returns a DateInterval for the last week
 func getDates() *costexplorer.DateInterval {
 	now := time.Now()
@@ -26,8 +36,6 @@ func getDates() *costexplorer.DateInterval {
 // covert string to float to string for formatting
 func formatNumber(s string) string {
 	f, _ := strconv.ParseFloat(s, 64)
-	// fmt.Printf("%.2f", f)
-	f = f / 100
 	return fmt.Sprintf("%.2f", f)
 }
 
@@ -52,6 +60,7 @@ func main() {
 
 	svc := costexplorer.New(sess)
 
+	// Make the API call to cost explorer
 	resp, err := svc.GetCostAndUsage((&costexplorer.GetCostAndUsageInput{
 		Metrics:     []*string{aws.String("BlendedCost")},
 		TimePeriod:  getDates(),
@@ -79,7 +88,7 @@ func main() {
 	var data [][]string
 	for i, group := range resp.ResultsByTime {
 		// fmt.Println(group)
-		fmt.Println(len(group.Groups))
+		// fmt.Println(len(group.Groups))
 		slice.Sort(group.Groups[:], func(i, j int) bool {
 			a, _ := strconv.ParseFloat(*group.Groups[i].Metrics["BlendedCost"].Amount, 64)
 			b, _ := strconv.ParseFloat(*group.Groups[j].Metrics["BlendedCost"].Amount, 64)
@@ -96,6 +105,25 @@ func main() {
 					if j < len(data) {
 						data[j] = append(data[j], dollas)
 					}
+				}
+			}
+		}
+	}
+	for i := range data {
+		for j := range data[i] {
+			fmt.Println(j)
+			//data[0][i] = addColor(data[0][i], colors["yellow"])
+			if j != 0 {
+				a, _ := strconv.ParseFloat(data[i][j], 64)
+				b, err := strconv.ParseFloat(data[i][j-1], 64)
+				if err != nil {
+					fmt.Println(err)
+				}
+				if a > b {
+					fmt.Printf("%v is > %v\n", a, b)
+					data[i][j] = addColor(data[i][j], colors["red"])
+				} else {
+					data[i][j] = addColor(data[i][j], colors["green"])
 				}
 			}
 		}
